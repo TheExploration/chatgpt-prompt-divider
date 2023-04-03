ee07de1fc3e2ff029d38ac958b89aa5b98e58cdd
Christopher Kumm <chrislkumm@protonmail.com>
2023-04-02 19:41:02 -0500

    feat: Add functionality to split and display ChatGPT prompts
    This commit introduces the core functionality of the ChatGPT Prompt
    Divider. It adds state management, user input handling, and prompt
    splitting logic. The application now takes user input, divides it into
    chunks that stay within the 4096-character limit, and displays the
    resulting chunks on the page, along with copy buttons for easy copying.
    
    TODO:
    - Add testing for the `getChunks` function
    - Add a `didCopy` state to each of the chunks, and change the color of
      the chunk after it has been copied.
    - Improve overall styling

--- README.md
+++ README.md
@@ -1,34 +1,5 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# ChatGPT Prompt Divider
 
-## Getting Started
+ChatGPT Prompt Divider is a simple, user-friendly web application built using Next.js. The primary purpose of this tool is to help users manage long text inputs that exceed the 4096-character limit for ChatGPT prompts. The application automatically divides the input into smaller chunks, ensuring that each chunk stays within the character limit while maintaining the integrity of the text.
 
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
-
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+This tool is particularly useful for those who work with ChatGPT and need to handle lengthy prompts without compromising the quality and structure of the content. By providing an efficient and easy-to-use interface, users can focus on their input text and let the application take care of splitting the content into manageable portions for ChatGPT.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,125 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 import styles from './index.module.scss'
 
 const Home: NextPage = () => {
-  return <div className={styles.base}>TODO</div>
+  const [input, setInput] = useState('')
+
+  const chunks = getChunks(input, 4096)
+
+  const handleCopy = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text)
+    } catch (err) {
+      console.error('Failed to copy text: ', err)
+    }
+  }
+
+  return (
+    <div className={styles.base}>
+      <div className={styles.container}>
+        {/* Heading and description */}
+        <h1 className={styles.heading}>ChatGPT Prompt Divider</h1>
+        <p className={styles.introParagraph}>
+          ChatGPT prompts are limited to 4096 characters. This tool will divide
+          your prompts into as many chunks are needed to stay under the
+          character limit. It will also modify the prompts so that ChatGPT will
+          wait for further input before responding to the split-up prompt.
+        </p>
+
+        {/* Input */}
+        <textarea
+          className={styles.textarea}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder='Enter your prompt here...'
+        />
+        {/* Output */}
+        <div className={styles.chunkList}>
+          {chunks.map((chunk, index) => (
+            <div className={styles.chunk} key={chunk.slice(0, 20)}>
+              <div className={styles.chunkText}>{chunk}</div>
+              <button
+                className={styles.copyButton}
+                onClick={() => handleCopy(chunk)}
+              >
+                Copy
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * Splits a given text string into smaller chunks of a defined maximum size
+ * without cutting words in the middle. The first chunk will include
+ * instructions for ChatGPT, and the last chunk will have a finished string
+ * appended. If the input text is shorter than the defined chunk size, it will
+ * be returned as the only chunk.
+ *
+ * @param {string} str - The input text string to be split into chunks.
+ * @param {number} chunkSize - The maximum number of characters allowed in each chunk.
+ * @returns {string[]} An array of chunks, where each chunk has a length less than or equal to the specified chunk size.
+ * @throws Will throw an error if the chunk size is too small to accommodate the instructions for ChatGPT.
+ */
+function getChunks(str: string, chunkSize: number): string[] {
+  // If the string is already less than or equal to the chunk size, then
+  // just return the string as the only chunk.
+  if (str.length <= chunkSize) {
+    return [str]
+  }
+
+  const instructions =
+    '\n\nInstructions for ChatGPT: This prompt has been truncated to fit within the character limit. Simply listen and respond with "Ok" until my last prompt, which will end with the string "(FINISHED)".'
+
+  const finishedString = '\n\n(FINISHED)'
+
+  if (chunkSize < instructions.length) {
+    throw new Error(
+      `The chunk size must be at least ${instructions.length} characters long to accommodate the instructions.`
+    )
+  }
+
+  const chunks = []
+
+  // The first chunk will be the first chunkSize characters of the input string
+  // minus the length of the instructions string.
+  const firstChunkSize = chunkSize - instructions.length
+  let endIndex = str.lastIndexOf(' ', firstChunkSize)
+  endIndex = endIndex === -1 ? firstChunkSize : endIndex
+  chunks.push(str.slice(0, endIndex) + instructions)
+
+  // The remaining chunks will be split up into chunks of chunkSize characters
+  // starting where the previous chunk left off.
+  let startIndex = endIndex
+  while (startIndex < str.length) {
+    endIndex = str.lastIndexOf(' ', startIndex + chunkSize)
+    endIndex =
+      endIndex === -1 || endIndex <= startIndex
+        ? startIndex + chunkSize
+        : endIndex
+
+    const chunk = str.slice(startIndex, endIndex)
+    chunks.push(chunk)
+
+    startIndex = endIndex
+  }
+
+  if (chunks[chunks.length - 1].length + finishedString.length <= chunkSize) {
+    chunks[chunks.length - 1] += finishedString
+  } else {
+    chunks.push(finishedString)
+  }
+
+  // Trim leading and trailing whitespace from each chunk.
+  for (let i = 0; i < chunks.length; i++) {
+    chunks[i] = chunks[i].trim()
+  }
+
+  return chunks
 }
 
 export default Home
