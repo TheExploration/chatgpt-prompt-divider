5bead8d07a4d3e83864ea6e7b902f385ea3af0f7
Christopher Kumm <chrislkumm@protonmail.com>
2023-04-06 20:16:03 -0500

    Add favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.scss'
 import '@fontsource/lato'
 import type { AppProps } from 'next/app'
+import SEO from '../components/SEO/SEO'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SEO />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
